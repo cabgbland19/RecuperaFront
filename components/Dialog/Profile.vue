3b8d8ecf704ad96043daea958161b22ce086fe0a
Santiago Yate <syate@uno27.co>
2022-11-04 12:38:20 -0500

    Profile design improved

--- components/Dialog/Profile.vue
+++ components/Dialog/Profile.vue
@@ -15,31 +15,39 @@
       </v-btn>
     </template>
 
-    <v-card min-height="250px" class="cash d-flex justify-center align-center">
-      <v-row>
-        <!-- Nombre -->
-        <v-col cols="12" class="text-center">
-          <span v-text="data.name" />
-        </v-col>
-        <!-- Documento -->
-        <v-col cols="12" class="text-center">
-          <span v-text="data.document" />
-        </v-col>
+    <v-card min-height="240px" class="d-flex justify-center align-center">
+      <v-row class="px-2">
         <!-- Campaña -->
-        <v-col cols="12" class="text-center">
+        <v-col
+          cols="12"
+          class="text-center primary"
+          style="position: absolute; top: 0; left: 0; right: 0"
+        >
           <span
-            v-text="data.campaign"
-            class="primary--text text-overline font-weight-bold"
+            v-text="convertFirstLetterUppercase(data.campaign)"
+            class="white--text font-weight-bold"
           />
         </v-col>
+        <!-- Nombre -->
+        <v-col cols="12" class="text-center mt-12 field-name">
+          <span v-text="convertFirstLetterUppercase(data.name)" />
+        </v-col>
+        <!-- Documento -->
+        <v-col cols="12" class="text-center mt-n5 primary--text">
+          <span v-text="data.document" />
+        </v-col>
+
         <!-- Rol -->
         <v-col cols="12" class="text-center">
           <v-chip color="white">
-            <span v-text="data.rol" />
-            <v-icon right color="primary"> mdi-shield-account </v-icon>
+            <v-icon left color="primary"> mdi-shield-account </v-icon>
+            <span
+              v-text="convertFirstLetterUppercase(data.rol)"
+              class="text-body-1"
+            />
           </v-chip>
-        </v-col>
-      </v-row>
+        </v-col> </v-row
+      ><datalist></datalist>
     </v-card>
   </v-menu>
 </template>
@@ -52,16 +60,38 @@ export default {
       default: () => {},
     },
   },
+  // computed: {
+  //   campaign() {
+  //     const text = this.data.campaign.toLowerCase();
+  //     return text[0].toUpperCase() + text.slice(1);
+  //   },
+  // },
+  methods: {
+    convertFirstLetterUppercase(val) {
+      const text = val.toLowerCase();
+      return text[0].toUpperCase() + text.slice(1);
+    },
+  },
 };
 </script>
 
 <style>
-.cash {
-  background: rgb(255, 255, 255);
+.field-name {
+  text-transform: capitalize !important;
+}
+/* .field-campaign {
+  text-transform: lowercase !important;
+}  */
+/* .field-campaign::first-letter {
+  text-transform: uppercase !important;
+} */
+/* .cash { */
+/* background: rgb(255, 255, 255);
   background: linear-gradient(
     0deg,
     rgba(255, 255, 255, 1) 0%,
     rgba(0, 0, 0, 0.38139005602240894) 100%
-  );
-}
+  ); */
+/* border: 5px solid red; */
+/* } */
 </style>
